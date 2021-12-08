ca008cbd2e1b0d51fa640f15b621881002396ff8
Shiva <l.shivaprasad.160@gmailcom>
2021-12-08 21:05:35 +0530

    user add With date comman

--- .idea/03-variables.sh
+++ .idea/03-variables.sh
@@ -15,5 +15,7 @@ echo a in currency = ${a}USD
 
 DATE = 2021-12-08
 echo Good morning ,welcome,Today date is ${DATE}
+DATE = $(date-%F)
+echo Good morning ,welcome,Today date is ${DATE}
 ADD=$((2+3))
 echo ADD = ${ADD}
