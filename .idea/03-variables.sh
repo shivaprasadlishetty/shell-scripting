73f7836dfb219d6324cbd5031e8c3d78c9df5dab
Shiva <l.shivaprasad.160@gmailcom>
2021-12-08 20:41:49 +0530

    Variables with currency

--- .idea/03-variables.sh
+++ .idea/03-variables.sh
@@ -4,3 +4,11 @@
 a=100 #Number
 b=apple #String
 C=true #Bool
+
+#Access the variable 2 ways
+# $ preeceding to the variable
+# $ preeceding to the variable and flower braces
+# best practice is 2 nd one
+
+echo a = $a
+echo a in currency = ${a}USD
