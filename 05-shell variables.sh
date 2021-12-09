50787b5c01a0c4d5845f583bcbfdcf0ddf37cd86
Shiva <l.shivaprasad.160@gmailcom>
2021-12-09 22:29:05 +0530

    add special variable

--- 05-shell variables.sh
+++ 05-shell variables.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
 
+# $0 is nothing but script name
 echo ${0}
+# $1 is the 1st arguments
 echo ${1}
+# $2 is the 2st arguments
 echo ${2}
-
+# $* & $@ to get all the inputs
 echo ${*}
 echo ${@}
 
+# $# is the number of imputs been parsed
 echo ${#}
